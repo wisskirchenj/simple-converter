bf4e3d7e4c80ff496f896f1083cf5a06bf4d254f
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2022-10-23 20:39:58 +0200

    final stage 5 completed - reuse branch logic of delete_def., tests

--- src/menu.bash
+++ src/menu.bash
@@ -2,6 +2,7 @@
 # converter stage 4
 
 FILE_NAME="definitions.txt"
+REGEX_NUMBER='^-?[0-9]+\.?[0-9]*$'
 
 function print_menu() {
   echo """
@@ -17,7 +18,6 @@ function parse_definition() {
   convert_command=${command_line[0]}
   convert_factor=${command_line[1]}
   regex_command='^[a-zA-Z]+_to_[a-zA-Z]+$'
-  regex_number='^-?[0-9]+\.?[0-9]*$'
 }
 
 function add_to_file() {
@@ -30,7 +30,7 @@ function add_definition() {
     echo "Enter a definition:"
     read -a command_line
     parse_definition
-    if [ $token_count -ne 2 ] || [[ ! $convert_command =~ $regex_command ]] || [[ ! $convert_factor =~ $regex_number ]]; then
+    if [ $token_count -ne 2 ] || [[ ! $convert_command =~ $regex_command ]] || [[ ! $convert_factor =~ $REGEX_NUMBER ]]; then
       echo "The definition is incorrect!"
     else
       correct_definition=true
@@ -54,23 +54,63 @@ function delete_line_from_file() {
   fi
 }
 
+function check_if_definitions() {
+    regex="^0 .+$"
+    if [ ! -e $FILE_NAME ] || [[ $(echo `wc $FILE_NAME`) =~ $regex ]]; then
+      echo "Please add a definition first!"
+      no_definition=1
+      return
+    fi
+    no_definition=0
+}
+
+function determine_line() {
+    echo $1
+    regex="^([0-9]+) .*$"
+    [[ $(echo `wc $FILE_NAME`) =~ $regex ]] && lines=${BASH_REMATCH[1]}
+
+    list_file_with_line_numbers
+    read line
+    while [[ ! $line =~ ^[0-9]+$ ]] || [ $line -gt $lines ]; do
+      echo "Enter a valid line number!"
+      read line
+    done
+}
+
 function delete_definition() {
-  regex="^0 .+$"
-  if [ ! -e $FILE_NAME ] || [[ $(echo `wc $FILE_NAME`) =~ $regex ]]; then
-    echo "Please add a definition first!"
+  check_if_definitions
+  if [ $no_definition == "1" ]; then
     return
   fi
-  echo "Type the line number to delete or '0' to return"
-  regex="^([0-9]+) .*$"
-  [[ $(echo `wc $FILE_NAME`) =~ $regex ]] && lines=${BASH_REMATCH[1]}
-
-  list_file_with_line_numbers
-  read line
-  while [[ ! $line =~ ^[0-9]+$ ]] || [ $line -gt $lines ]; do
-    echo "Enter a valid line number!"
-    read line
-  done
+  determine_line "Type the line number to delete or '0' to return"
   delete_line_from_file $line
+}
+
+function prompt_convert_value() {
+  echo "Enter a value to convert:"
+  read convert_value
+  while [[ ! $convert_value =~ $REGEX_NUMBER ]]; do
+    echo "Enter a float or integer value!"
+    read convert_value
+  done
+}
+
+function convert_from_line() {
+  result=$(echo "scale=2; $2 * $3" | bc -l)
+  printf "Result: %s\n" "$result"
+}
+
+function convert_units() {
+  check_if_definitions
+  if [ $no_definition == "1" ]; then
+    return
+  fi
+  determine_line "Type the line number to convert units or '0' to return"
+  if [ $line == "0" ]; then
+    return
+  fi
+  prompt_convert_value
+  convert_from_line `sed -n $line'p' $FILE_NAME` $convert_value
 }
 
 echo "Welcome to the Simple converter!"
@@ -78,7 +118,7 @@ print_menu
 read choice
 while [[ ! $choice =~ ^0|quit$ ]]; do
   case $choice in
-    "1") echo "Not implemented!";;
+    "1") convert_units;;
     "2") add_definition;;
     "3") delete_definition;;
     *) echo "Invalid option!";;
--- test/menu_test.bats
+++ test/menu_test.bats
@@ -27,12 +27,6 @@ function do_menu() {
     assert_output --partial "Goodbye!"
 }
 
-@test "1 0" {
-    run do_menu "1\n0"
-    assert_output --partial "Not implemented!"
-    assert_output --partial "Goodbye!"
-}
-
 @test "4 0" {
     run do_menu "4\n0"
     assert_output --partial "Invalid option!"
@@ -148,4 +142,65 @@ function do_menu() {
     run cat definitions.txt
     refute_output --partial "a_to_b 1.0"
     refute_output --partial "c_to_d 2.0"
+}
+
+@test "convert no file" {
+    run do_menu "1\n0"
+    assert_output --partial "Please add a definition first!"
+    assert_output --partial "Goodbye!"
+}
+
+@test "convert empty file" {
+    touch definitions.txt
+    run do_menu "1\n0"
+    assert_output --partial "Please add a definition first!"
+    assert_output --partial "Goodbye!"
+}
+
+@test "convert with 2 lines 0 chosen" {
+    echo "a_to_b 1.0" >> definitions.txt
+    echo "c_to_d 2.0" >> definitions.txt
+    run do_menu "1\n0\n0"
+    assert_output --partial "Type the line number to convert units or '0' to return"
+    assert_output --partial "1. a_to_b 1.0"
+    assert_output --partial "2. c_to_d 2.0"
+    assert_output --partial "Goodbye!"
+}
+
+@test "convert with 2 lines 3 chosen" {
+    echo "a_to_b 1.0" >> definitions.txt
+    echo "c_to_d 2.0" >> definitions.txt
+    run do_menu "1\n3\n0\n0"
+    assert_output --partial "Type the line number to convert units or '0' to return"
+    assert_output --partial "Enter a valid line number!"
+    assert_output --partial "Goodbye!"
+}
+
+@test "convert with 2 lines '' chosen" {
+    echo "a_to_b 1.0" >> definitions.txt
+    echo "c_to_d 2.0" >> definitions.txt
+    run do_menu "1\n\n0\n0"
+    assert_output --partial "Type the line number to convert units or '0' to return"
+    assert_output --partial "Enter a valid line number!"
+    assert_output --partial "Goodbye!"
+}
+
+@test "convert with 2 lines abc chosen" {
+    echo "a_to_b 1.0" >> definitions.txt
+    echo "c_to_d 2.0" >> definitions.txt
+    run do_menu "1\nabc\n0\n0"
+    assert_output --partial "Type the line number to convert units or '0' to return"
+    assert_output --partial "Enter a valid line number!"
+    assert_output --partial "Goodbye!"
+}
+
+@test "convert with 2 lines value 2.3 * 2 in 2nd try" {
+    echo "a_to_b 1.0" >> definitions.txt
+    echo "c_to_d 2.0" >> definitions.txt
+    run do_menu "1\n2\none\n2.3\n0"
+    assert_output --partial "Type the line number to convert units or '0' to return"
+    assert_output --partial "Enter a value to convert:"
+    assert_output --partial "Enter a float or integer value!"
+    assert_output --partial "Result: 4.60"
+    assert_output --partial "Goodbye!"
 }
